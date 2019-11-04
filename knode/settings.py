88b40ac4623efe788422b5562ecd1ab008f92aab
Krishnanunni <krishnanunnir97@gmail.com>
2019-11-04 08:02:06 +0000

    (setting) settings.py: removed secret key from settings.py and added a new secret key in env_setting.py

--- knode/settings.py
+++ knode/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*vul$9+^oao+-c)iv75q(urt*-_^olp2)&8r0gvg$7xfw4px+g'
 
 # Application definition
 
