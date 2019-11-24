6678828bac1d9fba9180744148c80d8e399a5e2e
Krishnanunni <krishnanunnir97@gmail.com>
2019-11-24 20:49:01 +0530

    (setting) settings.py: Updated the import for environment file settings

--- knode/settings.py
+++ knode/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from .environment_setting_values import *;
+from .env_setting import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
