693bf607ac03808c679d480ec6a0916e9a3682f0
Krishnanunni <krishnanunnir97@gmail.com>
2019-11-28 09:14:41 +0530

    (feature) Updated Post model for auto generation of slug field

--- feed/models.py
+++ feed/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 STATUS = (
@@ -17,6 +18,10 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-created_on']
-    
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
+
     def __str__(self):
         return self.title;
