8a640d789a83deac9c184943d76cf05d52f57465
Krishnanunni <krishnanunnir97@gmail.com>
2019-11-28 22:32:20 +0530

    Added offset values for delete and view, changed path for feed.
    Depending on the offset value the corresponding post can be viewed or deleted. Also, updated the url for feeds from 'feed/all' to 'feed'.

--- feed/urls.py
+++ feed/urls.py
@@ -4,7 +4,7 @@ from feed import views
 app_name = 'feed'
 urlpatterns = [
     path('add/', views.add_post, name= 'add_post'),
-    path('delete/', views.delete_post, name= 'delete_post'),
-    path('view/', views.view_post, name= 'view_post'),
-    path('all/', views.view_feed, name= 'view_feed'),
+    path('delete/<int:post_id>/', views.delete_post, name= 'delete_post'),
+    path('view/<int:post_id>/', views.view_post, name= 'view_post'),
+    path('', views.view_feed, name= 'view_feed'),
 ]
