ffbc5e1bb9e8609ffb2afcc2636c7da65de8672b
Krishnanunni <krishnanunnir97@gmail.com>
2019-11-28 21:13:59 +0530

    (feature) Added the add post functionality

--- feed/views.py
+++ feed/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import AddPostForm
+from .models import Post
 
 # Create your views here.
 def add_post(request, template_name= "feed/add_post.html"):
-    return render(request, template_name)
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        user = request.user
+        Post(title= title, content= content, author= user).save()
+        return HttpResponse('Value added successfully')
+    else:
+        addpostform = AddPostForm()
+        template_data = {'addpostform':addpostform}
+        return render(request, template_name, template_data)
 
 def delete_post(request, template_name= "feed/delete_post.html"):
     return render(request, template_name)
