60af2bed7932ed0f88313e83e9381a8f53255d26
Krishnanunni <krishnanunnir97@gmail.com>
2019-11-28 22:34:23 +0530

    (feature) Created view for deleting and  viewing post, and modified the corresponding templates

--- feed/views.py
+++ feed/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .forms import AddPostForm
+from django.contrib.auth.decorators import login_required
+
 from .models import Post
+from .forms import AddPostForm
 
 # Create your views here.
+@login_required
 def add_post(request, template_name= "feed/add_post.html"):
     if request.method == 'POST':
         title = request.POST.get('title')
@@ -16,11 +19,19 @@ def add_post(request, template_name= "feed/add_post.html"):
         template_data = {'addpostform':addpostform}
         return render(request, template_name, template_data)
 
-def delete_post(request, template_name= "feed/delete_post.html"):
-    return render(request, template_name)
+@login_required
+def delete_post(request, post_id, template_name= "feed/delete_post.html"):
+    Post.objects.get(id= post_id).delete()
+    return HttpResponse("The value has been deleted successfully")
 
-def view_post(request, template_name= "feed/view_post.html"):
-    return render(request, template_name)
+@login_required
+def view_post(request, post_id, template_name= "feed/view_post.html"):
+    post = Post.objects.get(id = post_id)
+    template_data = {'post': post}
+    return render(request, template_name, template_data)
 
+@login_required
 def view_feed(request, template_name= "feed/view_feed.html"):
-    return render(request, template_name)
+    allposts = Post.objects.all()
+    template_data = {'allposts': allposts}
+    return render(request, template_name, template_data)
