7e5a4135a4c1e9b3513f7c9f7903fcc08fb81257
Krishnanunni <krishnanunnir97@gmail.com>
2019-12-01 21:18:26 +0530

    (feature) Added 404 pages on access to restricted pages

--- feed/views.py
+++ feed/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import Http404
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
 
 from .models import Post
 from .forms import AddPostForm
@@ -25,29 +27,33 @@ def add_post(request, template_name= "feed/add_post.html"):
 
 @login_required
 def delete_post(request, post_id, template_name= "feed/delete_post.html"):
-    Post.objects.get(id= post_id).delete()
+    get_object_or_404(Post, id= post_id, author= request.user).delete()
     return HttpResponse("The value has been deleted successfully")
 
 @login_required
 def view_post(request, post_id, template_name= "feed/view_post.html"):
-    post = Post.objects.get(id= post_id,author= request.user)
+    post = get_object_or_404(Post, id= post_id, author= request.user)
     template_data = {'post': post}
     return render(request, template_name, template_data)
 
 @login_required
 def view_feed(request, template_name= "feed/view_feed.html"):
     publishedposts = Post.objects.filter(status= 1, author= request.user)
+    if not publishedposts.exists():
+        raise Http404("Sorry, this page doesn't exist")
     template_data = {'publishedposts': publishedposts}
     return render(request, template_name, template_data)
 
 def view_drafts(request, template_name= "feed/view_drafts.html"):
     draftposts = Post.objects.filter(status= 0, author= request.user)
+    if not draftposts.exists():
+        raise Http404("Sorry, this page doesn't exist")
     template_data = {'draftposts': draftposts}
     return render(request, template_name, template_data)
 
 def edit_draft(request, post_id, template_name= "feed/edit_draft.html"):
     if request.method == 'POST':
-        editpost = Post.objects.get(id= post_id)
+        editpost = get_object_or_404(Post, id= post_id, author= request.user)
         editpost.title = request.POST.get('title')
         editpost.content = request.POST.get('content')
         editpost.status = 0
@@ -56,7 +62,7 @@ def edit_draft(request, post_id, template_name= "feed/edit_draft.html"):
         editpost.save()
         return HttpResponse('Value added successfully')
     else:
-        draft_post = Post.objects.get(id= post_id);
+        draft_post = get_object_or_404(Post, id= post_id, author= request.user)
         editdraftpost= AddPostForm(initial= {'title':draft_post.title, 'content':draft_post.content })
         template_data = {'editdraftpost': editdraftpost}
         return render(request, template_name, template_data)
