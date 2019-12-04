af5e4d996c9ee88257b278f2b13c9cd8a792340d
Krishnanunni <krishnanunnir97@gmail.com>
2019-12-05 00:47:39 +0530

    (fix) The 'feed' namespace was being used twice

--- knode/urls.py
+++ knode/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('feed.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('authentication.urls', namespace='authentication')),
     path('feed/', include('feed.urls', namespace='feed')),
