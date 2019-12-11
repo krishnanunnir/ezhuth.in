d92b2a6fa29f1c329cacaf1e80e043f76262bfa8
Krishnanunni <krishnanunnir97@gmail.com>
2019-12-11 23:21:00 +0530

    (ui) Updated the ui for the feeds, drafts, add post  page using bootstrap

--- feed/forms.py
+++ feed/forms.py
@@ -5,3 +5,8 @@ class AddPostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title','content')
+
+    def __init__(self, *args, **kwargs):
+        super(AddPostForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
