fba1a449eb3ac2037e747d74c6b1a815031e2649
Krishnanunni <krishnanunnir97@gmail.com>
2019-12-11 09:45:07 +0530

    (feature) Added functionality to show form errors and other messages  for authentication app

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 
@@ -18,31 +19,30 @@ def render_login_page(request, template_name= "authentication/login_page.html"):
             login(request, user)
             return HttpResponseRedirect(login_success)
         else:
-            return HttpResponse("sorry, no such user")
-    else:
-        loginform = LoginForm()
-        template_data = {'loginform': loginform}
-        return render(request, template_name, template_data)
+            messages.warning(request, 'Invalid username or password')
+    loginform = LoginForm()
+    template_data = {'loginform': loginform}
+    return render(request, template_name, template_data)
 
 def render_signup_page(request, template_name= "authentication/signup_page.html"):
     if request.user.is_authenticated:
         return HttpResponseRedirect(login_success)
+    form = SignupForm()
     if request.method == "POST":
-        signupform = SignupForm(data= request.POST)
-        if signupform.is_valid():
-            user = signupform.save()
+        form = SignupForm(data= request.POST)
+        if form.is_valid():
+            user = form.save()
             user.set_password(user.password)
             user.save()
             return HttpResponse("user signed up")
-    else:
-        signupform = SignupForm()
-        template_data = {'signupform': signupform}
-        return render(request, template_name, template_data)
+    #case for error occurence or get request to the page
+    template_data = {'form': form}
+    return render(request, template_name, template_data)
 
 @login_required
 def logout_user(request):
     logout(request)
-    return HttpResponse("User logged out")
+    return HttpResponseRedirect(logout_success)
 
 @login_required
 def render_user_profile(request, template_name= "authentication/user_profile.html"):
