b4208f8717052488bd4ee8f406d38c74e0ff2d67
Krishnanunni <krishnanunnir97@gmail.com>
2019-12-14 12:40:32 +0530

    (refactor) Removed texteditor app

--- texteditor/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class MarkdownForm(forms.Form):
-    post_title = forms.CharField(label='title', max_length=256, widget=forms.TextInput(attrs={'autocomplete':'off','id':'post_title_id','placeholder':'Post Title','class':'form-control'}))
-    post_content = forms.CharField(widget=forms.Textarea(attrs={'autocomplete':'off','id':'post_content_id','placeholder':'Post Content','class':'form-control'}))
-    post_author = forms.CharField(label='Author', max_length=256, widget=forms.TextInput(attrs={'autocomplete':'off','id':'post_author_id','placeholder':'Post Author','class':'form-control'}))
--- texteditor/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from texteditor import views
-
-urlpatterns = [
-    path('', views.render_markdown_editor_template, name='render_markdown_editor_template'),
-]
