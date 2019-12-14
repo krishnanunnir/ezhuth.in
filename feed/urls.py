a7f7bb0e3574c86d4eac4a92d4c81d3ada00de55
Krishnanunni <krishnanunnir97@gmail.com>
2019-12-14 12:15:36 +0530

    (ui)(refactor) Added an 'all' page, chaged 'feed' 'views.py' for better readability, and made it common templates
    Added an all page for seeing posts by all users.
    Refactored 'views.py' to change the order of functions for better undersandability.
    Changed all methods to make use of common templates for drafts and posts

--- feed/urls.py
+++ feed/urls.py
@@ -3,13 +3,14 @@ from feed import views
 
 app_name = 'feed'
 urlpatterns = [
+    path('all/', views.view_all, name= 'view_all'),
     path('feed/', views.view_feed, name= 'view_feed'),
     path('feed/add/', views.add_post, name= 'add_post'),
-    path('feed/delete/<int:post_id>/', views.delete, name= 'delete_post'),
-    path('feed/edit/<int:post_id>', views.edit, name= 'edit_post'),
-    path('feed/view/<int:post_id>/', views.view, name= 'view_post'),
+    path('feed/view/<int:post_id>/', views.view_post, name= 'view_post'),
+    path('feed/edit/<int:post_id>', views.edit_post, name= 'edit_post'),
+    path('feed/delete/<int:post_id>/', views.delete_post, name= 'delete_post'),
     path('drafts/',views.view_drafts, name= 'view_drafts'),
-    path('drafts/edit/<int:post_id>', views.edit, name= 'edit_draft'),
-    path('drafts/delete/<int:post_id>', views.delete, name= 'delete_draft'),
-    path('drafts/view/<int:post_id>/', views.view, name= 'view_draft'),
+    path('drafts/view/<int:post_id>/', views.view_post, name= 'view_draft'),
+    path('drafts/edit/<int:post_id>', views.edit_post, name= 'edit_draft'),
+    path('drafts/delete/<int:post_id>', views.delete_post, name= 'delete_draft'),
 ]
