58a29d06b2f513a87a7746f1a8c2a5852115829a
Krishnanunni <krishnanunnir97@gmail.com>
2019-12-14 12:15:36 +0530

    (ui)(refactor) Added an 'all' page, chaged 'feed' 'views.py' for better readability, and made it common templates
    Added an all page for seeing posts by all users.
    Refactored 'views.py' to change the order of functions for better undersandability.
    Changed all methods to make use of common templates for drafts and posts

--- feed/views.py
+++ feed/views.py
@@ -34,8 +34,39 @@ def add_post(request, template_name= "feed/add_post.html"):
     template_data = {'form':form}
     return render(request, template_name, template_data)
 
+
 @login_required
-def delete(request, post_id):
+def edit_post(request, post_id, template_name= "feed/edit_post.html"):
+    post = get_object_or_404(Post, id= post_id, author= request.user)
+    if request.method == 'POST':
+        form = AddPostForm(request.POST) #Sanitizing data using AddPostForm since both of them use the same data
+        if form.is_valid():           
+            post.title = form.cleaned_data.get('title')
+            post.content = form.cleaned_data.get('content')
+            if 'publish' in request.POST:
+                post.status = 1
+                messages.info(request, 'Post added to feed')
+                redirect_url = feed_home
+            else:
+                post.status = 0
+                messages.info(request, 'Post updated in Drafts')
+                redirect_url = drafts_home
+            post.save()
+            return HttpResponseRedirect(redirect_url)
+    form = AddPostForm(initial= {'title':post.title, 'content':post.content })
+    template_data = {'form': form}
+    return render(request, template_name, template_data)
+
+
+@login_required
+def view_post(request, post_id, template_name= "feed/view_post.html"):
+    post = get_object_or_404(Post, id= post_id, author= request.user)
+    template_data = {'post': post}
+    return render(request, template_name, template_data)
+
+
+@login_required
+def delete_post(request, post_id):
     post = get_object_or_404(Post, id= post_id, author= request.user)
     post_status = post.status
     post.delete()
@@ -47,46 +78,27 @@ def delete(request, post_id):
         redirect_url = feed_home
     return HttpResponseRedirect(redirect_url)
 
-@login_required
-def view(request, post_id, template_name= "feed/view_post.html"):
-    post = get_object_or_404(Post, id= post_id, author= request.user)
-    template_data = {'post': post}
-    return render(request, template_name, template_data)
 
-@login_required
-def view_feed(request, template_name= "feed/view_feed.html"):
-    published_posts = Post.objects.filter(status= 1, author= request.user)
-    if not published_posts.exists():
+def view_all(request, template_name= "feed/view_posts.html"):
+    posts = Post.objects.filter(status= 1)
+    if not posts.exists():
         messages.info(request, "No content to display")
-    template_data = {'published_posts': published_posts}
+    template_data = {'posts': posts}
     return render(request, template_name, template_data)
 
+
 @login_required
-def view_drafts(request, template_name= "feed/view_drafts.html"):
-    draft_posts = Post.objects.filter(status= 0, author= request.user)
-    if not draft_posts.exists():
+def view_feed(request, template_name= "feed/view_posts.html"):
+    posts = Post.objects.filter(status= 1, author= request.user)
+    if not posts.exists():
         messages.info(request, "No content to display")
-    template_data = {'draft_posts': draft_posts}
+    template_data = {'posts': posts}
     return render(request, template_name, template_data)
 
 @login_required
-def edit(request, post_id, template_name= "feed/edit_draft.html"):
-    editpost = get_object_or_404(Post, id= post_id, author= request.user)
-    if request.method == 'POST':
-        form = AddPostForm(request.POST) #Sanitizing data using AddPostForm since both of them use the same data
-        if form.is_valid():           
-            editpost.title = form.cleaned_data.get('title')
-            editpost.content = form.cleaned_data.get('content')
-            if 'publish' in request.POST:
-                editpost.status = 1
-                messages.info(request, 'Post added to feed')
-                redirect_url = feed_home
-            else:
-                editpost.status = 0
-                messages.info(request, 'Post updated in Drafts')
-                redirect_url = drafts_home
-            editpost.save()
-            return HttpResponseRedirect(redirect_url)
-    form = AddPostForm(initial= {'title':editpost.title, 'content':editpost.content })
-    template_data = {'form': form}
+def view_drafts(request, template_name= "feed/view_posts.html"):
+    posts = Post.objects.filter(status= 0, author= request.user)
+    if not posts.exists():
+        messages.info(request, "No content to display")
+    template_data = {'posts': posts}
     return render(request, template_name, template_data)
