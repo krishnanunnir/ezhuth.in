15e9897c236c42008686374d9dff1883f495f816
Krishnanunni <krishnanunnir97@gmail.com>
2019-12-14 12:40:32 +0530

    (refactor) Removed texteditor app

--- texteditor/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .forms import MarkdownForm
-
-# Create your views here.
-def render_markdown_editor_template( request, template_name="texteditor/markdown_text_editor.html"):
-	""" Renders the text editor template for markdown view"""
-	markdownform = MarkdownForm()
-	template_render_data = {'markdownform':markdownform}
-	return render(request, template_name, template_render_data)
