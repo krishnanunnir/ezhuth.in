f508b2caaac46e40107437e0742d89c33be98fb1
Krishnanunni <krishnanunnir97@gmail.com>
2019-12-15 20:22:04 +0530

    (feature) Added confirm password in signup page

--- authentication/forms.py
+++ authentication/forms.py
@@ -10,6 +10,17 @@ class LoginForm(forms.ModelForm):
 
 class SignupForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password=forms.CharField(widget=forms.PasswordInput())
     class Meta():
         model = User
-        fields = ('username', 'password', 'email')
+        fields = ('username', 'password', 'confirm_password','email')
+
+    def clean(self):
+        cleaned_data = super(SignupForm, self).clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "password and confirm_password does not match"
+            )
