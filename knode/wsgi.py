fd6c4a8394c65fd4d55e42bdb58ef4a0daec4f6c
Krishnanunni R <krishnanunnir97@gmail.com>
2020-07-31 12:38:54 +0530

    [fix] Loadenv issue due to wrong path fixed

--- knode/wsgi.py
+++ knode/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+from dotenv import load_dotenv
 
 from django.core.wsgi import get_wsgi_application
-
+env_file = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),'.env')  
+load_dotenv(env_file)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'knode.settings.production')
 
 application = get_wsgi_application()
