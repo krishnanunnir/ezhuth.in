40d566b05182d01b83b810b5da068617c2b18dfd
Krishnanunni R <krishnanunnir97@gmail.com>
2020-07-31 22:50:31 +0530

    [enhancement] Moved back to sngle setting file

--- knode/wsgi.py
+++ knode/wsgi.py
@@ -13,6 +13,6 @@ from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
 env_file = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),'.env')  
 load_dotenv(env_file)
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'knode.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'knode.settings')
 
 application = get_wsgi_application()
