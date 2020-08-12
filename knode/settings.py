c8b1b49a6557526c9dbd0ff0d1f1c47dfa5345cb
Krishnanunni R <krishnanunnir97@gmail.com>
2020-08-12 23:22:13 +0530

    [fix] Setting the debug variable correctly

--- knode/settings.py
+++ knode/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 SECRET_KEY = os.getenv('SECRET_KEY')
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','localhost').split(',')
-DEBUG = os.getenv('DEBUG',False)
+DEBUG = os.getenv('DEBUG',False) and "True"
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),'.')
