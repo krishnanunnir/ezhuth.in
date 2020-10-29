4d25918e828c791a5635d7fbb1f4e16caed408c9
Krishnanunni R <krishnanunnir97@gmail.com>
2020-10-30 01:12:27 +0530

    [fix] Fix for not showing usernames with @ and other symbols

--- feed/urls.py
+++ feed/urls.py
@@ -1,5 +1,8 @@
-from django.urls import path, include
+from django.urls import path, register_converter
 from feed import views
+from . import converters
+
+register_converter(converters.UsernameConverter, 'username')
 
 app_name = 'feed'
 urlpatterns = [
@@ -8,6 +11,6 @@ urlpatterns = [
     path('add/', views.add_post, name= 'add_post'),
     path('view/<slug:post_slug>', views.view_post, name= 'view_post'),
     path('edit/<slug:post_slug>', views.edit_post, name= 'edit_post'),
-    path('user/<slug:username>', views.view_user, name= 'view_user'),
+    path('user/<username:username>', views.view_user, name= 'view_user'),
     path('delete/<slug:post_slug>', views.delete_post, name= 'delete_post'),
 ]
