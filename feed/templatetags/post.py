e794871e9b01c6d2dcbfbc241df1a3019ac8d44b
Krishnanunni R <krishnanunnir97@gmail.com>
2020-11-07 00:36:18 +0530

    [feature] Fixed description
    If post longer than 100px post clipped and shown, otherwise
    the whole post is shown

--- feed/templatetags/post.py
+++ feed/templatetags/post.py
@@ -3,7 +3,3 @@ from datetime import datetime, timezone
 from feed.models import Post, Like
 register = template.Library()
 
-@register.filter(name="post_description")
-def comment_count(post):
-    
-    return post.content[0:30]
