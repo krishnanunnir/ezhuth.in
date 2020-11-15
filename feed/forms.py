9280988fe37fdccc048f259d06ec22a2130baab2
Krishnanunni R <krishnanunnir97@gmail.com>
2020-11-15 22:37:32 +0530

    [feature] Added messages for home, viewpost and addpost

--- feed/forms.py
+++ feed/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import ImageTinymce, Post, Comment
-
+from django.utils.translation import ugettext_lazy as _
 class AddPostForm(forms.ModelForm):
     class Meta:
         model = Post
@@ -8,22 +8,28 @@ class AddPostForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'placeholder': 'ടൈറ്റിൽ'}),
         }
+        labels = {
+            "content": _("Post")
+        }
     def __init__(self, *args, **kwargs):
         super(AddPostForm, self).__init__(*args, **kwargs)
 
         for field in self.fields.values():
-            field.error_messages = {'required':'The field {fieldname} is required'.format(
+            field.error_messages = {'required':'The {fieldname} is empty.'.format(
                 fieldname=field.label)}
 
 class AddCommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('content',)
+        labels = {
+            "content": _("Comment")
+        }
     def __init__(self, *args, **kwargs):
         super(AddCommentForm, self).__init__(*args, **kwargs)
 
         for field in self.fields.values():
-            field.error_messages = {'required':'The field {fieldname} is required'.format(
+            field.error_messages = {'required':'{fieldname} is empty'.format(
                 fieldname=field.label)}
 
 class ImageUploadForm(forms.ModelForm):
