5bb154264b2b6b1d7f17e24ad34d923ff760c691
Krishnanunni R <krishnanunnir97@gmail.com>
2021-01-16 22:18:08 +0530

    [ui] Added quill editor back in view post

--- feed/templatetags/bleach_tags.py
+++ feed/templatetags/bleach_tags.py
@@ -4,7 +4,7 @@ from django import template
 from django.utils.safestring import mark_safe
 
 import bleach
-allowed_tags =['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i', 'li', 'ol', 'pre' ,'div','del','h1','br','p','span', 'strong', 'ul','img']
+allowed_tags =['a', 'abbr', 'acronym', 'b', 'blockquote', 'code', 'em', 'i', 'li', 'ol', 'pre' ,'div','del','h1','br','p','span', 'strong', 'ul','img','h2']
 allowed_attributes = {'a': ['href', 'title'], 'abbr': ['title'], 'acronym': ['title'],'img': ['src','alt','width','height']}
 
 register = template.Library()
