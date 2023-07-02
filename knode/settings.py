3c182207922c2202777b79f7f78cd3cec2e83f6e
krishnanunnir <krishnanunnir97@gmail.com>
2023-07-02 13:55:18 +0530

    url resolution issue fix

--- knode/settings.py
+++ knode/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = os.path.join(
 )
 
 # Define these values for your local setting by importing from this setting
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+    "default": dj_database_url.config(),
 }
 # Application definition
 
